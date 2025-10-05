1c0fad5ff335e615a0d0ab4b1f8dbf90bbdb2804
Oscar Palm <palmoscar4@gmail.com>
2025-10-05 18:42:57 +0200

    Styled card for search results

--- frontend/src/components/listObject/Album.tsx
+++ frontend/src/components/listObject/Album.tsx
@@ -2,26 +2,33 @@
 
 import { Album } from "@/types/dataTypes";
 import Image from "next/image";
+import styles from "./Card.module.css";
 
 export const AlbumCard = (album: Album) => {
 	const data: Album = album.album;
 	console.log(data);
 	return (
-		<div>
+		<div className={styles.card}>
 			<Image
 				src={data.images[1].url}
 				width={300}
 				height={300}
 				alt={`Image of ${data.name}`}
 			/>
-			<h2>{data.name}</h2>
-			<h4>
-				By:{" "}
-				{data.artists.length == 1
-					? data.artists[0].name
-					: data.artists.map((artist) => artist.name).join(", ")}
-				{` (${data.release_date.split("-")[0]})`}
-			</h4>
+			<ul>
+				<li>
+					<h2 className={styles.cardHeader}>{data.name}</h2>
+				</li>
+				<li>
+					<p>
+						By:{" "}
+						{data.artists.length == 1
+							? data.artists[0].name
+							: data.artists.map((artist) => artist.name).join(", ")}
+						{` (${data.release_date.split("-")[0]})`}
+					</p>
+				</li>
+			</ul>
 		</div>
 	);
 };
--- frontend/src/components/listObject/Artist.tsx
+++ frontend/src/components/listObject/Artist.tsx
@@ -2,18 +2,34 @@
 
 import { Artist } from "@/types/dataTypes";
 import Image from "next/image";
+import styles from "./Card.module.css";
 
+const prettyNum = (num: number, short = true) => {
+	const mil = 1000000;
+	const kil = 1000;
+	if (num > mil)
+		return Math.round((num * 100) / mil) / 100 + (short ? "M" : " Million");
+	if (num > kil)
+		return Math.round((num * 10) / kil) / 10 + (short ? "K" : " Thousand");
+};
 export const ArtistCard = (artist: Artist) => {
 	const data: Artist = artist.artist;
 	return (
-		<div>
+		<div className={styles.card}>
 			<Image
 				src={data.images[2].url}
 				width={160}
 				height={160}
 				alt={`Image of ${data.name}`}
 			/>
-			<h2>{data.name}</h2>
+			<ul>
+				<li>
+					<h2 className={styles.cardHeader}>{data.name}</h2>
+				</li>
+				<li>
+					<p>Followers: {prettyNum(data.followers, false)}</p>
+				</li>
+			</ul>
 		</div>
 	);
 };
